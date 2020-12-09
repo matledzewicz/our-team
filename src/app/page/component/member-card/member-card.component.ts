0c83ce243c2f1798e91dffc6509bb3a63ecf4939
Mateusz Łędzewicz <mateusz.ledzewicz@gft.com>
2020-12-09 19:28:32 +0100

    Load different images base on viewport size

--- src/app/page/component/member-card/member-card.component.ts
+++ src/app/page/component/member-card/member-card.component.ts
@@ -8,9 +8,20 @@ import { MemberCard } from 'src/app/api/page-content';
 })
 export class MemberCardComponent implements OnInit {
   @Input() card: MemberCard;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  calculateSrcSet(): string {
+    // TODO: Calculate it base on MemberCard imageUrl property
+    return `${this.card.imageUrl.w1080} 1080w, ${this.card.imageUrl.w400} 400w, ${this.card.imageUrl.w200} 200w`;
+  }
+
+  calculateSizes(): string {
+    // TODO: Calculate it base on MemberCard imageUrl property
+    return '(max-width: 700px) 400px, (max-width: 1250px) 200px, (max-width: 3300px) 400px, 1080px';
   }
 
 }
